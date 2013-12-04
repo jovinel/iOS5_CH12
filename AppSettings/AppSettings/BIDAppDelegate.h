0c52e258ae57fc1ed4ea18b483ba16977af5aff6
Jovi Nel <jnp.parilla@gmail.com>
2013-12-04 16:17:29 +0800

    First commit, initialize data for the project
    Signed-off-by: Jovi Nel <jnp.parilla@gmail.com>

---
+++ AppSettings/AppSettings/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  AppSettings
+//
+//  Created by Maria Alice C. Lim on 12/4/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppSettings/AppSettings/BIDMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDMainViewController.h
+//  AppSettings
+//
+//  Created by Maria Alice C. Lim on 12/4/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "BIDFlipsideViewController.h"
+
+@interface BIDMainViewController : UIViewController <BIDFlipsideViewControllerDelegate>
+
+@end
